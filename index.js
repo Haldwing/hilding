287140ba8f397ac6bb4cd228fee9e612b74eeebd
Haldwing <139230101+Haldwing@users.noreply.github.com>
2024-05-09 12:28:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ $(".biolink").on("mouseout", function(){
     $(".biolink").animate({opacity: 0.7})
    });
    
+   $(".livelink").on("mouseover", function(){
+    $(".livelink").animate({opacity: 1})
+   });
+   
+   $(".livelink").on("mouseout", function(){
+       $(".livelink").animate({opacity: 0.7})
+      });
